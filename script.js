4f241afa85255c9f170ea9b6ea7eb4c61c88e0a8
sofia amoroso <sofi79a0@stud.kea.dk>
2023-02-02 15:18:38 +0100

    only paper button is working

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ function start(){
     playerChoice();
 }
 
+const myArray = [
+    "paper",
+    "rock",
+    "scissors"
+];
+
+const randomComputerChoice = myArray[Math.floor(Math.random() * 3)];
+console.log(randomComputerChoice);
+
+
 function playerChoice(){
-    // add event lister for each button
     document.querySelector("button.rock").addEventListener("click", shaking);
     document.querySelector("button.scissors").addEventListener("click", shaking);
     document.querySelector("button.paper").addEventListener("click", shaking);
-    // makeRandomComputerChoice();
-}
-
-function makeRandomComputerChoice(){
-// math.random.something 0-2
-// changeHand();
 }
 
 function shaking(){
@@ -26,41 +29,48 @@ function shaking(){
     document.querySelector("div#player2").addEventListener("animationend", changeUserHand);
 }
 
+function changeComputerHand(){
+    console.log('change computer hand');
+    document.querySelector("div#player1").classList.remove("shake");
+    document.querySelector("div#player1").classList.add(randomComputerChoice);
+}
+
 function changeUserHand(){
     console.log('change user hand');
     document.querySelector("div#player2").classList.remove("shake");
-
     document.querySelector("div#player2").classList.add("paper");
     determineWinner();
 }
 
-function changeComputerHand(){
-    console.log('change computer hand');
-    document.querySelector("div#player1").classList.remove("shake");
-}
+let winner = ""
 
-
-
-let winner =""
+const user = "paper";
+const computer = randomComputerChoice;
+console.log(user);
+console.log(computer);
 
 function determineWinner(){
-    winner = "computer"
-    // compare variables
-    showWin();
-    // if sentences that sayd "if the result is this, the call this function"
+    if(user === computer) {
+        showDraw();
+    }
+    else if (computer == 'rock'){
+        showWin();
+    } 
+    else{
+        showLose();
+    }
 }
 
 function showWin(){
-
+    document.querySelector("div#win").classList.remove("hidden");
 }
 
 function showLose(){
-    
+    document.querySelector("div#lose").classList.remove("hidden");
 }
 
-
 function showDraw(){
-    
+    document.querySelector("div#draw").classList.remove("hidden");
 }
 
 
